fa665ff0c12c0c1b3844f0f55776bf7304923e2b
xLybert <felixgs2@hotmail.com>
2016-01-17 02:28:25 +0100

    Añadidas todas las preguntas

--- cuestionario.php
+++ cuestionario.php
@@ -20,9 +20,40 @@ and open the template in the editor.
                 <input type="radio" name="elemento" value="Hielo"> Hielo<br>
                 <input type="radio" name="elemento" value="Luz"> Luz<br>
             </fieldset>
-        </form>
-        <?php
-        
-        ?>
-    </body>
-</html>
+            <fieldset>
+                <legend>Pregunta Nº.2</legend> <br>
+                En el relato de "Las Reliquias de la Muerte", ¿cuál de las tres reliquias posée Dumbledore? <br>
+                <input type="radio" name="reliquia" value="Filosofal"> La piedra Filosofal<br>
+                <input type="radio" name="reliquia" value="Capa"> La capa de la invisibilidad<br>
+                <input type="radio" name="reliquia" value="Varita"> La Varita de Sáuco<br>
+                <input type="radio" name="reliquia" value="Caliz"> El Cáliz de fuego<br>
+            </fieldset>
+            <fieldset>
+                <legend>Pregunta Nº.3</legend> <br>
+                ¿Cuál de estos personajes de "Star Wars" mató a Lord Sidious? <br>
+                <input type="radio" name="pj" value="Vader"> Darth Vader<br>
+                <input type="radio" name="pj" value="Luke"> Luke Skywalker<br>
+                <input type="radio" name="pj" value="Solo"> Han Solo<br>
+                <input type="radio" name="pj" value="Kenobi"> Obi-Wan Kenobi<br>
+            </fieldset>
+            <fieldset>
+                <legend>Pregunta Nº.4</legend> <br>
+                ¿En qué año fue 1+1? <br>
+                    <input type="radio" name="simpsons" value="2"> ¿2? <br>
+                    <input type="radio" name="simpsons" value="para"> Pa k kieres saber eso jaja salu2<br>
+                    <input type="radio" name="simpsons" value="7"> 1+1 son sieteeeee....<br>
+                    <input type="radio" name="simpsons" value="ralph"> La respuesta es el FANTÁSTICO Ralph<br>
+            </fieldset>
+            <fieldset>
+                <legend>Pregunta Nº.5</legend>
+                ¿Qué jugador de waterpolo debe llevar el gorro número 1 a la hora de jugar un partido? <br>
+                    <input type="radio" name="water" value="captain"> El capitán<br>
+                    <input type="radio" name="water" value="boya"> El boya<br>
+                    <input type="radio" name="water" value="puerta"> El portero<br>
+                    <input type="radio" name="water" value="defensa"> El defensor de boya<br>
+            </fieldset>
+                </form>
+                <?php
+                ?>
+                </body>
+                </html>
