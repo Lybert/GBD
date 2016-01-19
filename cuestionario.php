505a12e87a4b79fb010d3c21f51c5b78975d9893
a021704062c <a021704062c@S04-PC11>
2016-01-19 19:00:10 +0100

    Versión inicial

--- cuestionario.php
+++ cuestionario.php
@@ -16,42 +16,42 @@ and open the template in the editor.
                 <fieldset>
                     <legend>Pregunta Nº.1</legend> <br>
                     La Alquimia se compone de cuatro elementos básicos: Tierra, Agua, Aire y ... <br>
-                    <input type="radio" name="elemento" value="Éter"> Eter<br>
-                    <input type="radio" name="elemento" value="Fuego"> Fuego<br>
-                    <input type="radio" name="elemento" value="Hielo"> Hielo<br>
-                    <input type="radio" name="elemento" value="Luz"> Luz<br>
+                    <input type="radio" name="a" value="Éter"> Eter<br>
+                    <input type="radio" name="a" value="Fuego"> Fuego<br>
+                    <input type="radio" name="a" value="Hielo"> Hielo<br>
+                    <input type="radio" name="a" value="Luz"> Luz<br>
                 </fieldset>
                 <fieldset>
                     <legend>Pregunta Nº.2</legend> <br>
                     En el relato de "Las Reliquias de la Muerte", ¿cuál de las tres reliquias posée Dumbledore? <br>
-                    <input type="radio" name="reliquia" value="Filosofal"> La piedra Filosofal<br>
-                    <input type="radio" name="reliquia" value="Capa"> La capa de la invisibilidad<br>
-                    <input type="radio" name="reliquia" value="Varita"> La Varita de Sáuco<br>
-                    <input type="radio" name="reliquia" value="Caliz"> El Cáliz de fuego<br>
+                    <input type="radio" name="b" value="Filosofal"> La piedra Filosofal<br>
+                    <input type="radio" name="b" value="Capa"> La capa de la invisibilidad<br>
+                    <input type="radio" name="b" value="Varita"> La Varita de Sáuco<br>
+                    <input type="radio" name="b" value="Caliz"> El Cáliz de fuego<br>
                 </fieldset>
                 <fieldset>
                     <legend>Pregunta Nº.3</legend> <br>
                     ¿Cuál de estos personajes de "Star Wars" mató a Lord Sidious? <br>
-                    <input type="radio" name="pj" value="Vader"> Darth Vader<br>
-                    <input type="radio" name="pj" value="Luke"> Luke Skywalker<br>
-                    <input type="radio" name="pj" value="Solo"> Han Solo<br>
-                    <input type="radio" name="pj" value="Kenobi"> Ben Solo Organa<br>
+                    <input type="radio" name="c" value="Vader"> Darth Vader<br>
+                    <input type="radio" name="c" value="Luke"> Luke Skywalker<br>
+                    <input type="radio" name="c" value="Solo"> Han Solo<br>
+                    <input type="radio" name="c" value="Kenobi"> Ben Solo Organa<br>
                 </fieldset>
                 <fieldset>
                     <legend>Pregunta Nº.4</legend> <br>
                     ¿En qué año fue 1+1? <br>
-                    <input type="radio" name="simpsons" value="2"> ¿2?<br>
-                    <input type="radio" name="simpsons" value="para"> Pa k kieres saber eso jaja salu2<br>
-                    <input type="radio" name="simpsons" value="7"> 1+1 son sieteeeee....<br>
-                    <input type="radio" name="simpsons" value="ralph"> La respuesta es el FANTÁSTICO Ralph<br>
+                    <input type="radio" name="d" value="2"> ¿2?<br>
+                    <input type="radio" name="d" value="para"> Pa k kieres saber eso jaja salu2<br>
+                    <input type="radio" name="d" value="7"> 1+1 son sieteeeee....<br>
+                    <input type="radio" name="d" value="ralph"> La respuesta es el FANTÁSTICO Ralph<br>
                 </fieldset>
                 <fieldset>
                     <legend>Pregunta Nº.5</legend> <br>
                     ¿Qué jugador de waterpolo debe llevar el gorro número 1 a la hora de jugar un partido? <br>
-                    <input type="radio" name="water" value="captain"> El capitán<br>
-                    <input type="radio" name="water" value="boya"> El boya<br>
-                    <input type="radio" name="water" value="puerta"> El portero<br>
-                    <input type="radio" name="water" value="defensa"> El defensor de boya<br>
+                    <input type="radio" name="e" value="captain"> El capitán<br>
+                    <input type="radio" name="e" value="boya"> El boya<br>
+                    <input type="radio" name="e" value="puerta"> El portero<br>
+                    <input type="radio" name="e" value="defensa"> El defensor de boya<br>
                 </fieldset> <br>
                 <input type="submit" value="Enviar">
             </div>
@@ -60,11 +60,11 @@ and open the template in the editor.
         // 1º. Lo primero es obtener los datos pasados en los campos anteriores
         // y almacenarlos en las variables correspondientes.
 
-        $elemento = $_POST['elemento'];
-        $reliquia = $_POST['reliquia'];
-        $pj = $_POST['pj'];
-        $simpsons = $_POST['simpsons'];
-        $water = $_POST['water'];
+        $elemento = $_POST['a'];
+        $reliquia = $_POST['b'];
+        $pj = $_POST['c'];
+        $simpsons = $_POST['d'];
+        $water = $_POST['e'];
 
         // 2º. Realizar la conexión con la Base de Datos.
 
@@ -73,7 +73,11 @@ and open the template in the editor.
             die("Fallo al conectar a MySQL: (" . $conexion->connect_errno . ") " . $conexion->connect_error);
         }
         
-        // 3º. 
+        // 3º. Preparamos la consulta SQL.
+        
+        $comprobar = "SELECT * "
+                ."FROM respuestas "
+                . "WHERE idRespuesta=?"
         ?>
     </body>
 </html>
