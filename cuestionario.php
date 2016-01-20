11f9cb512d6b08f2ec3040e5c5889d7dd67d218e
xLybert <felixgs2@hotmail.com>
2016-01-20 20:54:00 +0100

    Cuestionario revisado

--- cuestionario.php
+++ cuestionario.php
@@ -12,49 +12,6 @@ and open the template in the editor.
     <body>
         <h1>Examen</h1>
         <form action="calificador.php" method="post">
-            <div>
-                <fieldset>
-                    <legend>Pregunta Nº.1</legend> <br>
-                    La Alquimia se compone de cuatro elementos básicos: Tierra, Agua, Aire y ... <br>
-                    <input type="radio" name="a" value="Éter"> Eter<br>
-                    <input type="radio" name="a" value="Fuego"> Fuego<br>
-                    <input type="radio" name="a" value="Hielo"> Hielo<br>
-                    <input type="radio" name="a" value="Luz"> Luz<br>
-                </fieldset>
-                <fieldset>
-                    <legend>Pregunta Nº.2</legend> <br>
-                    En el relato de "Las Reliquias de la Muerte", ¿cuál de las tres reliquias posée Dumbledore? <br>
-                    <input type="radio" name="b" value="Filosofal"> La piedra Filosofal<br>
-                    <input type="radio" name="b" value="Capa"> La capa de la invisibilidad<br>
-                    <input type="radio" name="b" value="Varita"> La Varita de Sáuco<br>
-                    <input type="radio" name="b" value="Caliz"> El Cáliz de fuego<br>
-                </fieldset>
-                <fieldset>
-                    <legend>Pregunta Nº.3</legend> <br>
-                    ¿Cuál de estos personajes de "Star Wars" mató a Lord Sidious? <br>
-                    <input type="radio" name="c" value="Vader"> Darth Vader<br>
-                    <input type="radio" name="c" value="Luke"> Luke Skywalker<br>
-                    <input type="radio" name="c" value="Solo"> Han Solo<br>
-                    <input type="radio" name="c" value="Kenobi"> Ben Solo Organa<br>
-                </fieldset>
-                <fieldset>
-                    <legend>Pregunta Nº.4</legend> <br>
-                    ¿En qué año fue 1+1? <br>
-                    <input type="radio" name="d" value="2"> ¿2?<br>
-                    <input type="radio" name="d" value="para"> Pa k kieres saber eso jaja salu2<br>
-                    <input type="radio" name="d" value="7"> 1+1 son sieteeeee....<br>
-                    <input type="radio" name="d" value="ralph"> La respuesta es el FANTÁSTICO Ralph<br>
-                </fieldset>
-                <fieldset>
-                    <legend>Pregunta Nº.5</legend> <br>
-                    ¿Qué jugador de waterpolo debe llevar el gorro número 1 a la hora de jugar un partido? <br>
-                    <input type="radio" name="e" value="captain"> El capitán<br>
-                    <input type="radio" name="e" value="boya"> El boya<br>
-                    <input type="radio" name="e" value="puerta"> El portero<br>
-                    <input type="radio" name="e" value="defensa"> El defensor de boya<br>
-                </fieldset> <br>
-                <input type="submit" value="Enviar">
-            </div>
         </form>
     </body>
 </html>
